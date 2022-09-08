8cb221ecf0a9499f9a07f98f70d0348fbb0b5258
gabrielAnacletoo <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-09-08 20:57:13 -0300

    Create ObjetoAssign.js
    implementando propriedades diferentes em um object ja criado com assign

---
+++ folder/ObjetoAssign.js
@@ -0,0 +1,13 @@
+let carro = {
+  portas: 2,
+  portamalas: '200L',
+  motor: '2.0'
+  }
+let adicionais = {
+  tetosolar = true,
+  arcondicionado = true,
+}
+
+console.log(carro);
+object.assign(carro,adicionais);
+console.log(carro);
