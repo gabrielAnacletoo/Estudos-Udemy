3d74322c55f98306d078bcb7278baaaa0d966253
gabrielAnacletoo <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-09-08 21:19:06 -0300

    Create shiftEunshift.js
    .shift() remove o primero elemento de um array,
      .unshift() adiciona um ou mais elementos no inicio de uma array

---
+++ folder/shiftEunshift.js
@@ -0,0 +1,10 @@
+let carros = ['BMW', 'audi', 'vw', 'fiat'];
+ler removerPrimeiro = carros.shift();
+
+console.log(removerPrimeiro);
+console.log(carros);
+
+carros.unshift('Gurgel');
+
+console.log('carros');
+console.log('carros[0]');
