0de49e78ac1e1ade5ac92f3f2bf669e60d12fc3b
gabrielAnacletoo <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-09-08 21:12:57 -0300

    Create pushEPop.js
    o metodo .pop() remove o ultimo elemento da array, e .push() adiciona um elemento no fim do array

---
+++ folder/pushEPop.js
@@ -0,0 +1,10 @@
+let nomes = ['gabriel', 'isabela', 'maria', 'guilherme'];
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido);
+console.log(nomes);
+
+nomes.push('eliza');
+
+console.log(nomes);
