3913b7c3dc71dda3800df6ecbad10ef26bd283b5
gabrielAnacletoo <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-09-13 18:28:57 -0300

    Calculadora em javascript
    uma calculadora simples usando functions em javascript

---
+++ exerciciosArrayEobjetos/calculadora.js
@@ -0,0 +1,22 @@
+const calculadora = {
+    soma: function(a,b) {
+        return a + b;
+            },
+            subtrair: function(a,b) {
+                return a - b;
+                    },
+                    multiplicar: function(a,b) {
+                        return a * b;
+                            },
+                            dividir: function(a,b) {
+                                return a / b;
+                                    }
+}
+
+console.log(calculadora.soma(3,4));
+
+console.log(calculadora.subtrair(10,2));
+
+console.log(calculadora.multiplicar(7,4));
+
+console.log(calculadora.dividir(90,3));
