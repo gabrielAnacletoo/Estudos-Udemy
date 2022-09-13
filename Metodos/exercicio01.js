cea9a216d5cda51e1be4698522af7d68a7269ac3
gabrielAnacletoo <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-09-13 18:46:54 -0300

    Create exercicio01.js
    alterando raça de um cachorro no objeto

---
+++ Metodos/exercicio01.js
@@ -0,0 +1,21 @@
+const cachorro = {
+    raca: 'SRD',
+    uivar: function() {
+        console.log('auuuuuu');
+            },
+             rosnar: function () {
+                console.log('grrrrr');
+             },
+             setRaca: function (raca) {
+                this.raca = raca;
+             },
+             getRaca: function () {
+                return "A raça é " + this.raca;
+             }
+}
+
+console.log(cachorro.raca);
+cachorro.setRaca('Pastor Alemão');
+console.log(cachorro.raca);
+
+console.log(cachorro.getRaca());
