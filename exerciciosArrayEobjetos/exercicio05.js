e8807c227fcbb4231d156436145f2eaca65f806e
gabrielAnacletoo <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-09-13 18:31:28 -0300

    Create exercicio05.js

---
+++ exerciciosArrayEobjetos/exercicio05.js
@@ -0,0 +1,14 @@
+// VERIFICANDO DE EXISTE GABRIEL NA ARRAY 
+// IF 'SIM' CONSOLE LOG O NOME FOI ENCONTRADO
+// ELSE 
+
+let arr1 = [ 'Gabriel', 'Isabella', 'Aparecida','Wilson', 'Bia'];
+
+
+if(arr1.includes("Gabriel")) {
+    console.log("O Nome Foi Encontrado");
+} else {
+    console.log("O Nome não foi encontrando")
+}
+console.log(arr1[0]);
+
