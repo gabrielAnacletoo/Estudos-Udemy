bac8a72a71329c8c86a4985dbbc23ea21ad7df86
Gabriel Anacleto <109560707+gabrielAnacletoo@users.noreply.github.com>
2022-09-14 20:34:28 -0300

    Create GetterSetter.js

---
+++ prototype/GetterSetter.js
@@ -0,0 +1,23 @@
+class cachorro {
+    constructor (raca, cor) {
+        this.raca = raca;
+        this.cor = cor;
+    }
+    latir () {
+        console.log('au au');
+    }
+    get getCor() {
+        return this.cor;
+    }
+    set setCor(cor) {
+        this.cor = cor;
+    }
+}
+
+let pastor = new cachorro('Pastor Alemão', 'Sem cor');
+
+console.log(pastor);
+
+
+pastor.setCor = 'Marrom';
+console.log(pastor.getCor);
